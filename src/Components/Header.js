389dd31317892d640bfabf6cb4d94bc496fa1821
JO0022123 <142431315+JO0022123@users.noreply.github.com>
2024-03-07 15:26:13 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,7 @@ class Header extends React.Component {
     return (
       <>
       
-        <nav class="navbar navbar-expand-lg navbar navbar-dark bg-primary">
+        <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
           <div class="container-fluid">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
