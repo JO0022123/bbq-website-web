1fcfedec2800298db34a2bac8fe317f5b96fb791
JO0022123 <142431315+JO0022123@users.noreply.github.com>
2024-03-07 22:01:31 +0530

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -18,7 +18,7 @@ class Home extends React.Component {
                     <h1 class="display-3 text-white animated slideInLeft">Enjoy Our<br />Delicious Food</h1>
                     <p class="text-white animated slideInLeft mb-4 pb-2">Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet lorem sit clita duo justo magna dolore erat amet</p>
 
-                    <a href="#" class="btn btn-primary py-sm-3 px-sm-5 me-3 animated slideInLeft">Our Menu</a>
+                    <a href="#" class="btn btn-primary text-center py-sm-3 px-sm-5 me-3 animated slideInLeft">Our Menu</a>
 
 
                   </div>
